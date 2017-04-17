7b055dfe5b569b4b155d49710e2c5e9ced8a134c
Michael Cao <mike.x.cao@gmail.com>
2017-04-16 22:41:34 -0400

    Create utils.py

---
+++ cogs/utils.py
@@ -0,0 +1,40 @@
+import discord, aiohttp, bs4, lxml
+from bs4 import BeautifulSoup
+from discord.ext import commands
+
+class utils:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def isup(self, ctx):
+        temp = ctx.message.content.split(' ', 1)
+        if len(temp) == 1:
+            await self.bot.say("I'm sorry, but you need to give me a site to check.")
+        else:
+            site = temp[1]
+            if not site:
+                await self.bot.say("I'm sorry, but you need to give me a site to check.")
+
+            if '://' in site:
+                site = site.split('://')[1]
+            if not '.' in site:
+                site += ".com"
+            actualsite = site
+            try:
+                site = "http://isup.me/" + site
+                with aiohttp.ClientSession() as session:
+                    async with session.get(site) as resp:
+                        data = await resp.read()
+                    soup = BeautifulSoup(data, "lxml")
+                status = soup.find("div", {"id": "container"}).get_text()
+                if "It's just you." in status:
+                    await self.bot.say("It's just you. " + actualsite + " is up for me.")
+                else:
+                    await self.bot.say("Nope. " + actualsite + " is down for me.")
+            except Exception as e:
+                await self.bot.say("Hmm, something went wrong. Feel free to try again.")
+                print(e)
+
+def setup(bot):
+    bot.add_cog(utils(bot))
